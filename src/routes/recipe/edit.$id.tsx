506921b0b828b8d0407379a02012d83cbc8f60bd
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-04-11 10:10:19 +0200

    fix: edit navigation history issues

--- src/routes/recipe/edit.$id.tsx
+++ src/routes/recipe/edit.$id.tsx
@@ -7,7 +7,6 @@ import { NotFound } from '@/components/error/not-found'
 import { ScreenLayout } from '@/components/layout/screen-layout'
 import { Button } from '@/components/ui/button'
 import { Form } from '@/components/ui/form'
-import { Skeleton } from '@/components/ui/skeleton'
 import { Spinner } from '@/components/ui/spinner'
 import { getIngredientListOptions } from '@/features/ingredients/api/get-all'
 import { getRecipeListOptions } from '@/features/recipe/api/get-all'
@@ -32,58 +31,6 @@ const formatIngredientGroup = (group: RecipeIngredientGroup) => ({
     unitId: ingredient.unit?.id,
   })),
 })
-
-const EditRecipePending = () => (
-  <ScreenLayout title="Modifier la recette" withGoBack>
-    <div className="flex flex-col gap-4 p-4">
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-32" />
-        <Skeleton className="h-10 w-full" />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-24" />
-        <Skeleton className="h-10 w-full" />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-40" />
-        <Skeleton className="h-9 w-48" />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-36" />
-        <Skeleton className="h-48 w-full" />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-36" />
-        <Skeleton className="h-48 w-full" />
-      </div>
-
-      <div className="flex flex-col gap-2 pt-2">
-        <Skeleton className="h-4 w-44" />
-        <div className="flex flex-col gap-4 rounded-xl border p-4">
-          <Skeleton className="h-4 w-32" />
-          <Skeleton className="h-10 w-full" />
-          <Skeleton className="h-10 w-full" />
-          <Skeleton className="h-10 w-full" />
-        </div>
-        <Skeleton className="h-9 w-44" />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-28" />
-        <Skeleton className="h-64 w-full" />
-      </div>
-
-      <div className="flex flex-col justify-end gap-4 pt-6 md:flex-row">
-        <Skeleton className="h-10 w-full md:w-32" />
-        <Skeleton className="h-10 w-full md:w-40" />
-      </div>
-    </div>
-  </ScreenLayout>
-)
 
 const EditRecipePage = () => {
   const { id } = Route.useLoaderData()
@@ -123,11 +70,7 @@ const EditRecipePage = () => {
 
       await updateRecipe({ data: formData })
 
-      await router.navigate({
-        params: { id: id.toString() },
-        replace: true,
-        to: '/recipe/$id',
-      })
+      router.history.back()
     },
     validationLogic: revalidateLogic(),
     validators: {
@@ -168,7 +111,7 @@ const EditRecipePage = () => {
           initialVideo={recipe.video ? { id: recipe.video, url: getVideoUrl(recipe.video) } : undefined}
         />
         <div className="flex flex-col justify-end gap-4 pt-6 md:flex-row">
-          <Button disabled={isLoading} onClick={() => router.navigate({ to: '/' })} type="button" variant="outline">
+          <Button disabled={isLoading} onClick={() => router.history.back()} type="button" variant="outline">
             Annuler
           </Button>
           <form.AppForm>
@@ -204,5 +147,4 @@ export const Route = createFileRoute('/recipe/edit/$id')({
 
     return { id }
   },
-  pendingComponent: EditRecipePending,
 })
